a43bdb845f0daf0f8a9b831adafbbf4a0af0f22b
Denis856 <102806191+Denis856@users.noreply.github.com>
2022-08-28 18:11:29 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+   $('.page2_column').click(function(event){
+      $('.page2_column').toggleClass('down');
+   });
+   $('.page2_column').click(function(event){
+      $('.page2_column2').toggleClass('down');
+   });
+   $('.page2_column').click(function(event){
+      $('.page2_column3').toggleClass('down');
+   });
+   $('.page2_column').click(function(event){
+      $('.page2_column4').toggleClass('down');
+   });
+});
+
+$(document).ready(function(){
+   $('.page2_column2').click(function(event){
+      $('.page2_column').toggleClass('down');
+   });
+});
